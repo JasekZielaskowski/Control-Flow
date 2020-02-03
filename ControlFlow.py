3944e3e3fb92b7dcc86a591de6a89fca6f11b59f
Jasek Zielaskowski <02jasek.zielaskowski@gulllakecs.org>
2020-02-03 05:34:57 -0800

    Add Greeting

---
+++ ControlFlow.py
@@ -0,0 +1,12 @@
+# Date: 2.3.20
+# Greeting Function
+name = input("what is your name:  ")
+# Create Functions Here
+def greeting():
+    print ("Hi there " + name + "!")
+#Call Functions Here
+greeting()
+
+
+
+
