cddd840f150f3a2b083d0565c6bc7626c200e473
Jasek Zielaskowski <02jasek.zielaskowski@gulllakecs.org>
2020-02-04 05:03:35 -0800

    Add Function and Parameter

--- ControlFlow.py
+++ ControlFlow.py
@@ -1,24 +1,31 @@
+
 # Date: 2.3.20
-# Greeting Function
+# Programmer: Mr.Lange
 
 # Declare Global Variables here
 
-name = input("what is your name:  ")
+# name = input("what is your name:  ")
 x = 15
 
 
 # Create Functions Here
 
+# Greeting Function
 def greeting():
     print("Hi there " + name + "!")
     print("Very nice to meet you " + name)
-    print(x)
+
+# Functions and Local Variable x
 def printSomething():
     x = 3
     print(x)
-
+# Functions and Parameters
+def printNumber(age): #function name = printNumber with a PARAMETER of age
+    print(age)
 #Call Functions Here
 
-greeting()
-printSomething()
-print(x)
+# greeting()
+# printSomething()
+# print(x)
+printNumber(28)
+printNumber(38)
